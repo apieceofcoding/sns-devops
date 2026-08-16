2220194596a34f97d7937e8aec076c0119d97b0d
sanserif28 <sanserif28@gmail.com>
2026-08-16 20:51:12 +0900

    chore: 브랜치 이름 변경에 맞춰 run.sh 안내 문구 수정

--- run.sh
+++ run.sh
@@ -19,4 +19,4 @@ curl -fsS "http://prometheus.localhost/api/v1/rules?type=alert" | python3 -m jso
 
 echo
 echo "알림을 실제로 띄우려면 sns-app 에서 ./run.sh 로 에러 트래픽을 발생시키세요."
-echo "  git -C ../sns-app checkout part-8 && (cd ../sns-app && ./run.sh)"
+echo "  git -C ../sns-app checkout part-8-integration && (cd ../sns-app && ./run.sh)"
